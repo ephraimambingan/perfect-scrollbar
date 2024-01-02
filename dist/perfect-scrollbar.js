e736a21c196e020d51a6792a333f8939e16b4b72
Eph <koyaip.kuro@gmail.com>
2024-01-02 17:44:05 +0800

    fix non passive events

--- dist/perfect-scrollbar.js
+++ dist/perfect-scrollbar.js
@@ -117,7 +117,7 @@
       this.handlers[eventName] = [];
     }
     this.handlers[eventName].push(handler);
-    this.element.addEventListener(eventName, handler, false);
+    this.element.addEventListener(eventName, handler, { passive: true });
   };
 
   EventElement.prototype.unbind = function unbind (eventName, target) {
